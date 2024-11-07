fd03fef4b081fdc14c1330cef1119dee177d46aa
camdacoder <micahcscm@gmail.com>
2024-11-07 11:24:35 -0700

    Update genrescript.js

--- genrescript.js
+++ genrescript.js
@@ -2,3 +2,9 @@ function getMoodFromUrl() {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get('mood');
 }
+
+const mood = getMoodFromUrl();
+document.getElementById('comedy-link').href = `songselector.html?mood=${mood}&genre=comedy`;
+document.getElementById('drama-link').href = `songselector.html?mood=${mood}&genre=drama`;
+document.getElementById('action-link').href = `songselector.html?mood=${mood}&genre=action`;
+document.getElementById('adventure-link').href = `songselector.html?mood=${mood}&genre=adventure`;
